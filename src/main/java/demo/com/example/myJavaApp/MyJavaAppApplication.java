77f5ea3b3fbfbbe95f69d9ed80c7ec2912c33cb6
aman <aman.tyagi@tevvo.com>
2026-08-25 18:12:18 +0530

    First project with first commit

---
+++ src/main/java/demo/com/example/myJavaApp/MyJavaAppApplication.java
@@ -0,0 +1,13 @@
+package demo.com.example.myJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/com/example/myJavaApp/MyJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.com.example.myJavaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
